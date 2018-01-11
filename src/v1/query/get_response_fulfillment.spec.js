40a6c5cb4f193be3de6f0608b44a2a117e3c660b
Josh Salwen <jnsalwen@yahoo.com>
2018-01-11 13:45:40 -0800

    add getResponseFulfillment method

--- src/v1/query/get_response_fulfillment.spec.js
+++ src/v1/query/get_response_fulfillment.spec.js
@@ -7,25 +7,26 @@ const
 ;
 
 describe('V1.Query', () => {
-  it('getResponseResult() should return {} if no parameter is passed in', () => {
-    expect(QueryUtils.getResponseResult()).to.deep.equal({});
+  it('getResponseFulfillment() should return {} if no parameter is passed in', () => {
+    expect(QueryUtils.getResponseFulfillment()).to.deep.equal({});
   });
 
-  it('getResponseResult() should return {} if null parameter is passed in', () => {
-    expect(QueryUtils.getResponseResult(null)).to.deep.equal({});
+  it('getResponseFulfillment() should return {} if null parameter is passed in', () => {
+    expect(QueryUtils.getResponseFulfillment(null)).to.deep.equal({});
   });
 
-  it('getResponseResult() should return {} if {} parameter is passed in', () => {
+  it('getResponseFulfillment() should return {} if {} parameter is passed in', () => {
     const
       apiaiResponse = {
         result: {}
       },
       expected = {}
     ;
-    expect(QueryUtils.getResponseResult(apiaiResponse)).to.deep.equal(expected);
+
+    expect(QueryUtils.getResponseFulfillment(apiaiResponse)).to.deep.equal(expected);
   });
 
-  it('getResponseResult() should return object if full parameter is passed in', () => {
+  it('getResponseFulfillment() should return object if full fulfillment is passed in', () => {
     const
       apiaiResponse = {
         "id": "119a93ae-0e20-4df9-a595-97c239205de1",
@@ -77,41 +78,13 @@ describe('V1.Query', () => {
         "sessionId": "2bf17fa6-7758-4cf6-8525-c88a08c87293"
       },
       expected = {
-      	"source": "agent",
-      	"resolvedQuery": "my address is 126 Summer Terrace Ln Atlanta GA 30350",
-      	"action": "",
-      	"actionIncomplete": false,
-      	"parameters": {
-      		"address": "126 Summer"
-      	},
-      	"contexts": [{
-      		"name": "test-address",
-      		"parameters": {
-      			"address": "126 Summer",
-      			"address.original": [
-      				"126 Summer",
-      				"Terrace Ln",
-      				"Atlanta GA 30350"
-      			]
-      		},
-      		"lifespan": 5
-      	}],
-      	"metadata": {
-      		"intentId": "fde97fe3-316d-4b99-aa0e-67660931ed07",
-      		"webhookUsed": "false",
-      		"webhookForSlotFillingUsed": "false",
-      		"intentName": "unit-test-address-gather"
-      	},
-      	"fulfillment": {
-      		"speech": "",
-      		"messages": [{
-      			"type": 0,
-      			"speech": ""
-      		}]
-      	},
-      	"score": 1
-      }
+    		"speech": "",
+    		"messages": [{
+    			"type": 0,
+    			"speech": ""
+    		}]
+    	}
     ;
-    expect(QueryUtils.getResponseResult(apiaiResponse)).to.deep.equal(expected);
+    expect(QueryUtils.getResponseFulfillment(apiaiResponse)).to.deep.equal(expected);
   });
 });
--- src/v1/query/index.js
+++ src/v1/query/index.js
@@ -3,6 +3,7 @@
 exports.getResponseContext = require('./get_response_context');
 exports.getResponseContexts = require('./get_response_contexts');
 exports.getResponseContextParameters = require('./get_response_context_parameters');
+exports.getResponseFulfillment = require('./get_response_fulfillment')
 exports.getResponseResult = require('./get_response_result');
 exports.getResultParameter = require('./get_result_parameter');
 exports.getResultParameters = require('./get_result_parameters');
