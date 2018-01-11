5b763c1875d29fa68702d1a919cde14af181b8a8
Josh Salwen <jnsalwen@yahoo.com>
2018-01-11 12:41:18 -0800

    add getResultParameters

--- src/v1/query/index.js
+++ src/v1/query/index.js
@@ -5,4 +5,5 @@
 //exports.getResponseContextParameters = require('./get_response_context_parameters');
 exports.getResponseResult = require('./get_response_result');
 //exports.getResultParameter = require('./get_result_parameter');
+exports.getResultParameters = require('./get_result_parameters');
 //exports.mergeContexts = require('./merge_contexts');
